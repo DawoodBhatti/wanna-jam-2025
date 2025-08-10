b09fe9ab7c824d04063d3c50c43e895d53a146d5
Ali <d.a.bhatti@hotmail.com>
2025-08-10 18:44:22 +0100

    resource HUD placeholders

--- scenes + scripts/resource_control.gd
+++ scenes + scripts/resource_control.gd
@@ -1,5 +1,13 @@
 extends Node2D
 
+var stone_count : int = 0
+var wood_count : int = 0
+var food_count : int = 0
+var pop_count : int = 0
 
 func _ready() -> void:
-	print("i am the resource controller, hello :)")
+	print("initial resources: ")
+	print("stone: ", stone_count)
+	print("wood: ", wood_count)
+	print("wood: ", food_count)
+	print("population: ", pop_count)
